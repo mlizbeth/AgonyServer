4322fb33e7b72f4611f028cd3fe773f57906e69c
braybrandon <44496622+braybrandon@users.noreply.github.com>
2018-11-02 19:03:58 -0400

    Sprites displayed to screen and accepts keyboard movements

--- src/main/java/io/valhala/agonyserver/Game.java
+++ src/main/java/io/valhala/agonyserver/Game.java
@@ -1,44 +1,78 @@
 package io.valhala.agonyserver;
 
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 
+import io.valhala.agonyserver.framework.entity.charactermovement;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Game extends Application{
+	private HashMap<KeyCode, Boolean> keys = new HashMap<>();
+	Image image = new Image("/images/witch.png");
+	ImageView imageView = new ImageView(image);
+	charactermovement player = new charactermovement(imageView);
+	static Pane root = new Pane();
 	public static void main(String[] args) {
 		launch (args);
 	}
 	
-	static String title = "Agony-servers";
-	public void start(Stage primaryStage) throws FileNotFoundException {
-		Image image = new Image(new FileInputStream("src\\main\\java\\io\\valhala\\agonyserver\\framework\\entity\\rpg.jpg"));
-		primaryStage.setTitle(title);
-		Button btn = new Button();
-		btn.setText("Welcome to Agony");
-		btn.setOnAction(new EventHandler<ActionEvent>(){
-			
-			public void handle(ActionEvent e) {
-				System.out.println(title);
+	public void update() {
+		if (isPressed(KeyCode.UP)) {
+			player.animation.play();
+			player.animation.setOffsetY(96);
+			player.moveY(-2);
 			}
-		});
+		else if (isPressed(KeyCode.DOWN)) {
+			player.animation.play();
+			player.animation.setOffsetY(0);
+			player.moveY(2);
+			}
+		else if (isPressed(KeyCode.RIGHT)) {
+			player.animation.play();
+			player.animation.setOffsetY(64);
+			player.moveX(2);
+			}
+		else if (isPressed(KeyCode.LEFT)) {
+				player.animation.play();
+				player.animation.setOffsetY(32);
+				player.moveX(-2);
+				}
+		else
+			player.animation.stop();
+	}
+	
+	public boolean isPressed(KeyCode key) {
+		return keys.getOrDefault(key, false);
+	}
+	
+	public void start(Stage primaryStage) throws FileNotFoundException {
+		root.setPrefSize(600, 600);
+		root.getChildren().addAll(player);
 		
-		Group root = new Group();
-		ImageView play = new ImageView(image);
-		play.setViewport(new Rectangle2D(0,47,30,47));
-		root.getChildren().add(btn);
-		root.getChildren().add(play);
-		primaryStage.setScene(new Scene( root, 640, 480));
+		Scene scene = new Scene(root);
+		scene.setOnKeyPressed(event ->keys.put(event.getCode(), true));	
+		scene.setOnKeyReleased(event-> {
+			keys.put(event.getCode(), false);
+		});
+		AnimationTimer timer = new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				update();
+			}
+		};
+		timer.start();
+
+		
+		primaryStage.setTitle("Agony-Servers");
+		primaryStage.setScene(scene);
 		primaryStage.show()
 ;		
 	}
--- src/main/java/io/valhala/agonyserver/Main.java
+++ src/main/java/io/valhala/agonyserver/Main.java
@@ -4,7 +4,6 @@ package io.valhala.agonyserver;
 public class Main {
 
 	public static void main(String[] args) {
-	Game newgame = new Game();
 
 	}
 
