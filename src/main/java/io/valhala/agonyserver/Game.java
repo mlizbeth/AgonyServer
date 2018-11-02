b8bc6731313adb7238e39d8a8b97cd073a5d69db
braybrandon <44496622+braybrandon@users.noreply.github.com>
2018-11-02 10:18:38 -0400

    Updated to use JavaFX opposed to swing

--- src/main/java/io/valhala/agonyserver/Game.java
+++ src/main/java/io/valhala/agonyserver/Game.java
@@ -1,17 +1,39 @@
 package io.valhala.agonyserver;
 
-import javax.swing.*;
 
-public class Game extends JFrame{
-	static String title = "Agony-servers";
-	public Game() {
-		super(title);
-		System.out.println(title);
-		setSize(640,480);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Game extends Application{
+	public static void main(String[] args) {
+		launch (args);
 	}
-}
+	
+	static String title = "Agony-servers";
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle(title);
+		Button btn = new Button();
+		btn.setText("Welcome to Agony");
+		btn.setOnAction(new EventHandler<ActionEvent>(){
+			
+			public void handle(ActionEvent e) {
+				System.out.println(title);
+			}
+		});
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		primaryStage.setScene(new Scene( root, 640, 480));
+		primaryStage.show()
+;		
+	}
+		
+	}
+
 
 		
