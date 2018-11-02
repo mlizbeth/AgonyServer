83138d6887f9ed50479d8f7f169e02bcb9530ad2
braybrandon <44496622+braybrandon@users.noreply.github.com>
2018-11-02 16:06:24 -0400

    sprite uploader

--- src/main/java/io/valhala/agonyserver/Game.java
+++ src/main/java/io/valhala/agonyserver/Game.java
@@ -1,12 +1,18 @@
 package io.valhala.agonyserver;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Game extends Application{
@@ -15,7 +21,8 @@ public class Game extends Application{
 	}
 	
 	static String title = "Agony-servers";
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws FileNotFoundException {
+		Image image = new Image(new FileInputStream("src\\main\\java\\io\\valhala\\agonyserver\\framework\\entity\\rpg.jpg"));
 		primaryStage.setTitle(title);
 		Button btn = new Button();
 		btn.setText("Welcome to Agony");
@@ -26,8 +33,11 @@ public class Game extends Application{
 			}
 		});
 		
-		StackPane root = new StackPane();
+		Group root = new Group();
+		ImageView play = new ImageView(image);
+		play.setViewport(new Rectangle2D(0,47,30,47));
 		root.getChildren().add(btn);
+		root.getChildren().add(play);
 		primaryStage.setScene(new Scene( root, 640, 480));
 		primaryStage.show()
 ;		
